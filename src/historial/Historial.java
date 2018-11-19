2a2f7a1d82ac8f31c915bc3d3c1e93befff9c868
Andres Lopez <andres.lopez@unmsm.edu.pe>
2018-11-19 00:50:03 -0500

    Casi listo

---
+++ src/historial/Historial.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package historial;
+
+/**
+ *
+ * @author AnthonyDC
+ */
+public class Historial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventanaInicio v = new ventanaInicio();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
